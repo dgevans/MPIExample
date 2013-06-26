51149972fb5719d886a527ab901282db6e627829
David Evans <dgevans42@gmail.com>
2013-06-25 20:27:34 -0400

    Fixed bug in MPI fitting

--- bellman.py
+++ bellman.py
@@ -26,16 +26,19 @@ def approximateValueFunction(V,Para):
     n = len(Para.domain)
     m = n/s
     
-    mydomain = Para.domain[rank*m:min((rank+1)*m,n)]#split up the domain
+    mydomain = Para.domain[rank*m+min(rank,r):(rank+1)*m+min(rank+1,r)]#split up the domain
     myV = zeros(m)
     
     #get the value at each point in my domaain
     myV[0:len(mydomain)] = hstack(map(V,mydomain))
     
-    V = zeros(s*m)
-    comm.Allgather(myV,V)
-    
-    return Spline(Para.domain,V[0:len(Para.domain)],Para.deg)
+    Vs = w.gather(myV)
+    if rank == 0:
+        Vs = hstack(Vs)
+        Vf = Spline(Para.domain,Vs,Para.deg)
+    else:
+        Vf = None
+    return w.bcast(Vf)
     
     
     
